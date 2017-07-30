b3684ce227cfc6fe9dae096578d2c1b5dd172ff2
Stephan Balmer <sb@cis.ch>
2017-07-30 13:31:14 +0200

    it compiles

--- überkompensation.hs
+++ überkompensation.hs
@@ -4,41 +4,51 @@ import System.Environment
 
 import Data.WAVE
 
+type Frame = [Double]
+
 main = do
-	(inFile:outFile) <- getArgs
-	übercompensate inFile outFile
+	(inFile:outFile:[]) <- getArgs
+	inWave <- getWAVEFile inFile
+	putWAVEFile outFile (übercompensate inWave)
 
-übercompensate inPath outPath = do
-	inWave <- getWAVEFile inPath
-	let inHeader = waveHeader inWave
-	let inFrames = map (map sampleToDouble) (waveSamples inWave)
+übercompensate :: WAVE -> WAVE
+übercompensate inWave = 
+	let
+		inHeader = waveHeader inWave
+		inFrames :: [Frame]
+		inFrames = map (map sampleToDouble) (waveSamples inWave)
+		
+		flipper :: (Double, Double) -> Double
+		flipper (last, current) =
+			if
+				 abs (current - last) >= 1
+			then
+				(-1) * signum current * (2 - abs current)
+			else 
+				current
 
-	let flipper (last, current) =
-		if
-			 abs (current - last) >= 1
-		then
-			(-1) * sign current * (2 - abs current)
-		else 
-			current
+		findAbsMax :: (Frame, Double) -> Frame -> (Frame, Double)
+		findAbsMax (lastFrame, maxSample) currentFrame = 
+			let 
+				compensated = map flipper (zip lastFrame currentFrame)
+				currentMax :: Double
+				currentMax = maximum $ map abs compensated
+			in
+				(compensated, max maxSample currentMax)
 
-	let findAbsMax (lastFrame, maxSample) currentFrame = 
-		let 
-			compensated = map flipper (zip lastFrame currentFrame)
- 		in
-			(compensated, max(maxSample, maximum $ map abs compensated))
+		maxSample :: Double
+		maxSample = snd $ foldl findAbsMax (head inFrames, 0) inFrames
 
-	let maxSample = snd $ foldl findAbsMax (head inFrames, 0) inFrames
-	let normalization = (/maxSample)
+		compensateFrame :: Frame -> (Frame, [Frame]) -> (Frame, [Frame])
+		compensateFrame currentFrame (lastFrame, normalizedFrames) =
+			let
+				compensated = map flipper (zip lastFrame currentFrame)
+			in
+				(compensated, compensated : normalizedFrames)
 
-	let normalizeFrame (lastFrame, normalizedFrames) currentFrame =
-		let
-			compensated = map flipper (zip lastFrame currentFrame)
-		in
-			(compensated, map normalization compensated : normalizedFrames)
+		compensatedFrames = snd $ foldr compensateFrame (head inFrames, []) inFrames
 
-	let normalizedFrames = snd $ foldr compensateFrame (head inFrames, []) inFrames
-
-	let outFrames = map (map doubleToSample) normalizedFrames
-
-	putWAVEFile outFile outFrames
+		normalization = (/maxSample)
+	in
+		WAVE { waveHeader = inHeader, waveSamples = map (map (doubleToSample . normalization)) compensatedFrames }
 
