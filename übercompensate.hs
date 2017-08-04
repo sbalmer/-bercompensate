3f472f028cb56e7f93eca0e2d64e9f73f6fe8c39
Stephan Balmer <sb@cis.ch>
2017-08-04 17:51:58 +0200

    use minimum energy detection

--- übercompensate.hs
+++ übercompensate.hs
@@ -4,7 +4,24 @@ import System.Environment
 
 import Data.WAVE
 
-type Frame = [Double]
+import Data.List
+
+type Sample = Double
+type Frame = [Sample]
+type Stream = [Sample]
+type Window = [Sample] 
+
+
+
+
+energyRec :: Double -> Sample -> Window -> Double
+energyRec en last [] = en
+energyRec en last (x:xs) = energyRec (en + abs (last - x)) x xs 
+
+energy :: Window -> Double
+energy [] = 0
+energy (x:xs) = energyRec 0 x xs
+
 
 main = do
 	(inFile:outFile:[]) <- getArgs
@@ -16,25 +33,49 @@ main = do
 		inHeader = waveHeader inWave
 		inFrames :: [Frame]
 		inFrames = map (map sampleToDouble) (waveSamples inWave)
-		
-		flipper :: (Double, Double) -> Double
-		flipper (last, current) =
-			if abs (current - last) >= 1.5
-			then (-1) * signum current * (2 - abs current)
-			else current
-		
-		compensateFrame last [] = []
-		compensateFrame last (current:rest) =
-			let compensated = map flipper (zip last current)
-			in compensated : compensateFrame compensated rest
+		inStreams = transpose inFrames
 
-		compensatedFrames = compensateFrame (head inFrames) inFrames
+		processSample :: Window -> [Window] -> Double -> (Window, Window, [Window])
+		processSample base candidates sample = 
+			let
+				nextBase = sample:base
+				baseEnergy :: Double
+				baseEnergy = energy nextBase
+				terminationCandidates = map (sample : ) candidates
+				bestTermination = sort $ zip (map energy terminationCandidates) terminationCandidates
+				invertedSample = signum sample * (abs sample - 2)
+				-- Assume that overflows don't last long and discard long candidates
+				sufficientlyShortCandidates :: [Window]
+				sufficientlyShortCandidates = filter ((<10).length) candidates
+				nextCandidates :: [Window]
+				nextCandidates = (invertedSample : base) : map (invertedSample : ) sufficientlyShortCandidates 
+				-- Discard candidates that are much worse than the base case. This should keep the candidates list empty most of the time.
+				goodCandidates = filter ((< baseEnergy) . (+ 0.1) . energy) nextCandidates
+			in
+				case bestTermination of
+					-- correct overflow: if we find a run of inversions that is better than the base, take that
+					(en, bestTermCandidate):_ | en < baseEnergy -> (reverse $ tail bestTermCandidate, [sample], [])
+					_ -> case goodCandidates of
+						-- base case: there are no inversion candidates; output base and the current sample becomes the new base
+						[] -> (reverse base, [sample], [])
+						-- continue with candidates: inconclusive because no candidate terminated better than base
+						newCandidates -> ([], nextBase, newCandidates)
+					
+		processStream :: Window -> [Window] -> Stream -> Stream
+		processStream base _ [] = base
+		processStream base candidates (current:rest) =
+			output ++ processStream nextBase nextCandidates rest
+				where (output, nextBase, nextCandidates) = processSample base candidates current
 
-		-- maxSample = maximum $ map maximum compensatedFrames
-		maxSample = 1.1
+		reflowedStreams :: [Stream]
+		reflowedStreams = map (\(first:rest) -> processStream [first] [] rest) inStreams
+		reflowedFrames = transpose reflowedStreams
+	
+		maxSample = foldr1 max $ foldr1 max reflowedFrames 
+		-- maxSample = 1.1
 		normalization = (/maxSample)
 
-		outFrames = map (map (doubleToSample . normalization)) compensatedFrames
+		outFrames = map (map (doubleToSample . normalization)) reflowedFrames
 	in
 		WAVE { waveHeader = inHeader, waveSamples = outFrames }
 
